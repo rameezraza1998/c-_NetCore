c14ef012438481116faa7a490237f4c146c000b1
Syed Muhammad Rameez Raza <I_Rameez@trakker.com.pk>
2025-08-18 11:31:15 +0500

    Day1&2

---
+++ HelloCsharp/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+
+   // static variables jab banata hai jab huma values fixed rkhni hoti ha aur sharabale ho sab ma
+   static int myNum = 27;
+   static string myName = "Rameez";
+   static bool myBool = true;
+   static double temp = 36.6;
+
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(myNum);
+        Console.WriteLine(myName);
+        if (myBool)
+        {
+            Console.WriteLine("hello if");
+        }
+        Console.WriteLine(temp);
+    }
+}
