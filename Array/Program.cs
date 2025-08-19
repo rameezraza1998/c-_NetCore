9f1df2e906fcdbee1f73e2512d40ad6f982c190b
Syed Muhammad Rameez Raza <I_Rameez@trakker.com.pk>
2025-08-19 12:35:22 +0500

    Array with methods

---
+++ Array/Program.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+        //Console.WriteLine("Original array:");
+
+
+        //for loop to iterate through the array
+
+        //for (int i = 0; i < cars.Length; i++)
+        //    {
+        //        Console.WriteLine(cars[i]);
+        //    }
+
+        //cars[0] = "Camaro";
+        //Console.WriteLine("Changed array:");
+        //for (int i = 0; i < cars.Length; i++)
+        //{
+        //    Console.WriteLine(cars[i]);
+        //}
+
+
+        // foreach loop to iterate through the array
+
+
+        //foreach(string car in cars )
+        //{
+        //    Console.WriteLine(car);
+        //}
+
+
+
+        // Sort Method in array
+        //Array.Sort(cars);
+        //Console.WriteLine("Sorted array:");
+        //foreach (string car in cars)
+        //{
+        //    Console.WriteLine(car);
+        //}
+
+
+        //int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        //Console.WriteLine("Original array:");
+        //foreach(int i in nums)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        //Array.Reverse(nums);
+        //foreach (int i in nums)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        //Console.WriteLine(nums.Max());
+        //Console.WriteLine(nums.Min());
+        //Console.WriteLine(nums.Sum());
+
+
+        // Multi-dimensional array
+
+        //int[,] numbers = { {  1, 2, 6}, {  4, 5, 6,},
+        //                  {  7, 8, 9,} };
+
+        //Console.WriteLine("Multi-dimensional array:");
+        //// printing multi-dimensional array
+
+        //for(int i = 0; i<numbers.GetLength(0); i++)
+        //{
+        //    for(int j = 0; j< numbers.GetLength(1); j++)
+        //    {
+        //        Console.Write(numbers[i, j]);
+        //    }
+        //    Console.WriteLine();
+        //}       
+
+
+
+
+
+
+    }
+}
