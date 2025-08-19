65f214264930020f6f4bd1e3802af6651745dcca
Syed Muhammad Rameez Raza <I_Rameez@trakker.com.pk>
2025-08-19 14:00:09 +0500

    methods

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{   
+
+
+    static int add(int a ,int b)
+    {
+        return a + b;
+    }
+
+    public string GetName()
+    {
+        return "Rameez" ;
+    }
+
+    // another method without return type
+
+    private void Hello()
+    {
+        Console.WriteLine("Hello from another method");
+    }
+
+    private static void Main(string[] args)
+    {
+       
+
+        int result = add(6,6);
+        Console.WriteLine($"the sum of addition is: {result}");
+
+        Program p = new Program();
+
+        p.Hello();
+       Console.WriteLine(p.GetName());
+    }
+}
