f9d53e6f0b0109451bceb2e5cce45dadb55a9510
Syed Muhammad Rameez Raza <I_Rameez@trakker.com.pk>
2025-08-27 18:03:27 +0500

    interface task1 completed

---
+++ InterfaceTask1/InterfaceTask1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+interface CheckValueAtIndex
+{
+
+    int[] MyNum { get; set; }
+
+
+
+}
+
+class CheckValue : CheckValueAtIndex
+{
+    public int[] MyNum { get; set; }
+
+    public CheckValue(int mynum)
+    {        
+        MyNum = new int[] { 1, 2, 3, 4, 5 };
+    }
+}
+
+class Program
+{
+    private static void Main(string[] args)
+    {   
+        int indeval = Console.Read();
+        CheckValue obj = new CheckValue(indeval);
+        Console.WriteLine($"Value is: {obj.MyNum[1]}");
+        //Console.WriteLine("Hello, World!");
+    }
+}
