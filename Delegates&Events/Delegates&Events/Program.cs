6d26e032b14e0856063e3fa1d2fc64b0a0498f1a
Syed Muhammad Rameez Raza <I_Rameez@trakker.com.pk>
2025-09-03 14:53:39 +0500

    delegates completed now starting events

--- Delegates&Events/Delegates&Events/Program.cs
+++ Delegates&Events/Delegates&Events/Program.cs
@@ -67,11 +67,11 @@ public class Calculator
     //    Console.WriteLine();
     //}
 
-    public MathOperation  Op = delegate (int a, int b)
-    {
-       Console.WriteLine($"Division: {a / b}");
-        Console.WriteLine();
-    };
+    //public MathOperation  Op = delegate (int a, int b)
+    //{
+    //   Console.WriteLine($"Division: {a / b}");
+    //    Console.WriteLine();
+    //};
 
 }
 
@@ -79,16 +79,51 @@ public class Calculator
 class Program
 {
 
+    public static List<int> FilterList(List<int> numbers, Predicate<int> filter)
+    {
+
+        List<int> result = new List<int>();
+
+        //Console.WriteLine(filter);
+
+        foreach (var number in numbers)
+        {
+            if (filter(number))
+            {
+               
+                result.Add(number);
+            }
+        }
+
+        return result;  
+    }
+
 
 
     private static void Main(string[] args)
     {
 
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        //foreach (var number in numbers) 
+        //{
+        //    Console.WriteLine(number);
+        //}
+
+        List<int> even = FilterList(numbers,num => num % 2 == 0);
+
+        Console.WriteLine($"Even Numbers: {string.Join(", ", even)}");
+
+
+        List<int> odd = FilterList(numbers, num => num % 2 != 0);
+
+        Console.WriteLine($"Even Numbers: {string.Join(", ", odd)}");
+
         //myFirstDelegate myFirstDelegate = delegate (string message)
         //{
         //    Console.WriteLine(message);
         //};
-        MyClass myClass = new MyClass();
+        //MyClass myClass = new MyClass();
 
         //GreetUser greetUser = myClass.SayHello;
         //greetUser+= myClass.SayGoodbye;
@@ -112,21 +147,59 @@ class Program
         //notify warning = myClass.ShowWarning;
         //Console.WriteLine("Hello, World!");
 
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
 
         //MathOperation operation = calculator.Add;
         //operation += calculator.Subtract;
         //operation += calculator.Multiply;
-        MathOperation operation = calculator.Op;
-        operation(10, 2);
+        //MathOperation operation = calculator.Op;
+        //operation(10, 2);
 
 
+        //always void return nai karta action
+
+        //Action<int, int> add = (a, b) =>
+        //{
+        //    Console.WriteLine($"Addition: {a + b}");
+        //};
+
+        //add(10, 20);
+
+        //Action<int, int> subtract = (a, b) =>
+        //{
+        //    Console.WriteLine($"Subtraction: {a - b}");
+        //};
+        //subtract(10, 5);
+
+        //Action<int, int> mul = (a,b) =>
+        //{
+        //    Console.WriteLine($"Multiplivation: {a*b}");
+        //};
+        //mul(2, 3);
+
+        //Action<int> cube = (a) =>
+        //{
+
+        //    Console.WriteLine($"the cube of {a}: {a*a*a}");
+        //};
+        //cube(3);
 
 
+        // must have return type func
+
+        //Func<int, int,int > add = (a, b) => a + b;
+
+        //int result = add(10, 10);
+        //Console.WriteLine($"Addition: {result}");
+
+        //Func<int, int, int> subtract = (a, b) => a - b;
+
+        //Console.WriteLine($"Subtraction: {subtract(20, 10)}");
 
 
+        //Func<int, int, int> Product = (a, b) => a * b;
 
-
+        //Console.WriteLine($"Product: {Product(10, 10)}");
 
 
 
--- Delegates&Events/Delegates&Events/obj/Debug/net8.0/Delegates&Events.AssemblyInfo.cs
+++ Delegates&Events/Delegates&Events/obj/Debug/net8.0/Delegates&Events.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Delegates&Events")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5c78283ca61ef47e93926759d8ec3a1c928e0dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fc7d871ac4701665123dfc90e4287e225240e3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Delegates&Events")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Delegates&Events")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
