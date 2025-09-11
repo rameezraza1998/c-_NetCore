655b193459b55c5825ef13dcd0f879d1416bf323
Syed Muhammad Rameez Raza <I_Rameez@trakker.com.pk>
2025-09-11 17:58:18 +0500

    LINQ started

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+
+
+    private static void Main(string[] args)
+    {
+        int[] data  = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        var greaterThanThree = data.Where(n => n > 3).OrderByDescending(n => n);
+        Console.WriteLine(greaterThanThree);
+
+
+        foreach (var number in greaterThanThree)
+        {
+            Console.WriteLine(number);
+        }
+
+        //Console.WriteLine("Hello LINQ !! ");
+    }
+}
