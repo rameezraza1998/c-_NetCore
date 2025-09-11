a9a831cb5f99d3dc525d64d8f603d2b81554492f
Syed Muhammad Rameez Raza <I_Rameez@trakker.com.pk>
2025-09-11 17:34:33 +0500

    indexer completed & LINQ started

---
+++ Indexers/Indexers/Program.cs
@@ -0,0 +1,46 @@
+﻿class MyClass
+{
+    private int[] data = new int[5];
+
+
+    public int this[int index] 
+    {
+        get { return data[index];}
+        set { data[index] = value;}
+    }
+}
+
+class ReadOnlyIndexer
+{
+    private string[] names = { "Ramis", "Ali", "Romaisa" };
+
+    public string this[int index]
+    {
+        get { return names[index]; }
+    }
+
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {   
+        ReadOnlyIndexer obj = new ReadOnlyIndexer();
+
+        Console.WriteLine(obj[0]); // Output: Ramis 
+
+
+        //MyClass obj = new MyClass();
+
+        //obj[0] = 10;
+        //obj[1] = 20;
+        //obj[2] = 30;
+        //obj[3]= 40;
+        //obj[4] = 50;
+
+
+        //Console.WriteLine(obj[0]); // Output: 10
+
+        //Console.WriteLine("Indexers");
+    }
+}
