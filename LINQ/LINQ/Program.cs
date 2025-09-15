5fed2895353aa2bf506692d7fe5b78b04f0343b1
Syed Muhammad Rameez Raza <I_Rameez@trakker.com.pk>
2025-09-15 12:32:42 +0500

    LINQ started

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -1,19 +1,59 @@
 ﻿internal class Program
 {
+    List<string> students = new List<string>();
+
+    public void AddStudent(string Stdname)
+    {
+        string name = Stdname;
+        students.Add(name);
+    }
+
+    public void DisplayStudent()
+    {
+
+        foreach (var student in students)
+        { 
+        Console.WriteLine(student);
+        }
+    
+    }
+
+    public void GetStudentWithFirstletter(char letter)
+    { 
+            var result = from student in students
+                         where student.StartsWith(letter)
+                         select student;
+
+        foreach (var student in result)
+        { 
+            Console.WriteLine(student);
+        }
+    }
 
 
     private static void Main(string[] args)
     {
-        int[] data  = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        //int[] data  = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-        var greaterThanThree = data.Where(n => n > 3).OrderByDescending(n => n);
-        Console.WriteLine(greaterThanThree);
+        //var greaterThanThree = data.Where(n => n > 3).OrderByDescending(n => n);
+        //Console.WriteLine(greaterThanThree);
 
 
-        foreach (var number in greaterThanThree)
-        {
-            Console.WriteLine(number);
-        }
+        //foreach (var number in greaterThanThree)
+        //{
+        //    Console.WriteLine(number);
+        //}
+
+        Program program = new Program();
+        program.AddStudent("Ramis");
+        program.AddStudent("Qasim");
+        program.AddStudent("Raza");
+        program.AddStudent("Ali");
+        Console.WriteLine("Student List:");
+        program.DisplayStudent();
+
+        Console.WriteLine("Students with first letter R:");
+        program.GetStudentWithFirstletter('R');
 
         //Console.WriteLine("Hello LINQ !! ");
     }
--- LINQ/LINQ/obj/Debug/net8.0/LINQ.AssemblyInfo.cs
+++ LINQ/LINQ/obj/Debug/net8.0/LINQ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a831cb5f99d3dc525d64d8f603d2b81554492f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d28d57cc68d2d3f80e7498cd450d12f6ec17856")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
