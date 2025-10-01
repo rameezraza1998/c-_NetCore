1c33acb8a54dd36e869eac37dc225d585ec5220e
Syed Muhammad Rameez Raza <I_Rameez@trakker.com.pk>
2025-10-01 15:23:26 +0500

    exception handling finished

---
+++ Last Topics/Last Topics/Program.cs
@@ -0,0 +1,62 @@
+﻿public class CheckAge
+{
+
+    public int Age;
+    public CheckAge(int age)
+    {
+        Age = age;
+    }
+
+    public void getAge()
+    {
+
+        if(Age < 18)
+        {
+            throw new ArgumentException("Age is not valid");
+        }
+    }
+    
+
+  
+}
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            CheckAge C1 = new CheckAge(19);
+
+            Console.WriteLine(C1.Age);
+            C1.getAge();
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Your code is executed");
+        }
+
+       
+
+        //try
+        //{
+        //    int x = 10; int y = 2;
+        //    int result = x / y;
+        //    Console.WriteLine($"Result: {result}");
+        //}
+        //catch (Exception e)
+        //{
+        //    Console.WriteLine($"Cannot divide by Zero {e.Message}");
+        //}
+        //finally
+        //{
+        //    Console.WriteLine("The code is terminated");
+        //}
+
+
+    }
+}
